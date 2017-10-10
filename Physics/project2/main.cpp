98b5428bc79d1d02b98e1762116dd18a244f7f01
Dan's Laptop <dkorsah15@aol.co.uk>
2017-10-10 12:02:52 +0100

    Correct usage of body/particle class for multiple particles

--- Physics/project2/main.cpp
+++ Physics/project2/main.cpp
@@ -39,8 +39,8 @@ int main()
 	// create application
 	Application app = Application::Application();
 	app.initRender();
-	Application::camera.setCameraPosition(glm::vec3(0.0f, 5.0f, 20.0f));
-			
+	Application::camera.setCameraPosition(glm::vec3(0.0f, 3.0f, 10.0f));
+
 	// create ground plane
 	Mesh plane = Mesh::Mesh();
 	// scale it up x5
@@ -50,45 +50,46 @@ int main()
 
 	// create particle
 	std::vector<Particle> particles;
-	int particleNum = 10;
-	for (int i = 0; i < particleNum; i++) 
+	int particleNum = 300;
+	for (int i = 0; i < particleNum; i++)
 	{
 		Particle p = Particle::Particle();
 		particles.push_back(p);
 		//scale it down (x.1), translate it up by 2.5 and rotate it by 90 degrees around the x axis
-		particles[i].translate(glm::vec3(0.0f, 5.0f, 0.0f));
-		particles[i].scale(glm::vec3(0.5f, 0.5f, 0.5f));
-		particles[i].rotate((GLfloat) M_PI_2, glm::vec3(0.0f, 1.0f, 0.0f));
+		particles[i].setPos(glm::vec3(0.0f, 4.0f, 0.0f));
+		//particles[i].scale(glm::vec3(0.5f, 0.5f, 0.5f));
+		//particles[i].rotate((GLfloat) M_PI_2, glm::vec3(0.0f, 1.0f, 0.0f));
 		particles[i].getMesh().setShader(Shader("resources/shaders/core.vert", "resources/shaders/core_blue.frag"));
-		
+
+
+		//initial velocty
+		particles[i].setVel(glm::vec3(sin(i), 0.0f, cos(i)));
 	}
 
 	// time
-	GLfloat firstFrame = (GLfloat) glfwGetTime();
-	
+	GLfloat firstFrame = (GLfloat)glfwGetTime();
 
-	glm::vec3 velocity;
-	velocity = glm::vec3(1, 0, 0.8f);
-
-	struct Cube 
+	struct Cube
 	{
 		glm::vec3 origin = glm::vec3(-2.5f, 0.0f, -2.5f);
 		glm::vec3 bound = glm::vec3(2.5f, 5.0f, 2.5f);
 	};
 
 	Cube cube;
+	glm::vec3 gravity = glm::vec3(0.0f, -9.8f, 0.0f);
 
 	// Game loop
 	while (!glfwWindowShouldClose(app.getWindow()))
 	{
-		for(int i = 0; i < particleNum; i++)
+		// Set frame time
+		GLfloat currentFrame = (GLfloat)glfwGetTime() - firstFrame;
+		// the animation can be sped up or slowed down by multiplying currentFrame by a factor.
+		currentFrame *= 1.5f;
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		for (int i = 0; i < particleNum; i++)
 		{
-			// Set frame time
-			GLfloat currentFrame = (GLfloat)glfwGetTime() - firstFrame;
-			// the animation can be sped up or slowed down by multiplying currentFrame by a factor.
-			currentFrame *= 1.5f;
-			deltaTime = currentFrame - lastFrame;
-			lastFrame = currentFrame;
 
 			/*
 			**	INTERACTION
@@ -101,46 +102,46 @@ int main()
 			**	SIMULATION
 			*/
 			glm::vec3 Fg;
-			glm::vec3 gravity = glm::vec3(0, -9.8, 0);
 			float mass = 5;
 			Fg = gravity * mass;
 
+
 			//set acceleration
-			//particles[i].setAcc(gravity * deltaTime);
-			glm::vec3 acceleration = (gravity * deltaTime);
-			velocity += acceleration;
-
-			for (int i = 0; i < 3; i++)
-			{
-				if (particles[i].getTranslate()[3][i] < cube.origin[i])
-				{
-					particles[i].setPos(i, cube.origin[i]);
-					velocity[i] *= -0.7f;
-				}
-
-				if (particles[i].getTranslate()[3][i] > cube.bound[i])
-				{
-					particles[i].setPos(i, cube.bound[i]);
-					velocity[i] *= -0.7f;
-				}
-			}
+			particles[i].setAcc(gravity);
+			particles[i].setVel(particles[i].getVel() +particles[i].getAcc() * deltaTime);
 
 			//update the particle instance using translate and passing the velocity vector
-			particles[i].translate(velocity * deltaTime);
+			particles[i].translate(particles[i].getVel() * deltaTime);
+
+
+			for (int j = 0; j < 3; j++)
+			{
+				if (particles[i].getTranslate()[3][j] < cube.origin[j])
+				{
+					particles[i].setPos(j, cube.origin[j]);
+					particles[i].getVel()[j] *= -0.7f;
+				}
+
+				if (particles[i].getTranslate()[3][j] > cube.bound[j])
+				{
+					particles[i].setPos(j, cube.bound[j]);
+					particles[i].getVel()[j] *= -0.7;
+				}
+			}
 		}
 
 		/*
-		**	RENDER 
-		*/		
+		**	RENDER
+		*/
 		// clear buffer
 		app.clear();
 		// draw groud plane
 		app.draw(plane);
 		// draw particles
-		for(int i = 0; i < particleNum; i++)
+		for (int i = 0; i < particleNum; i++)
 		{
 			app.draw(particles[i].getMesh());
-		}			
+		}
 
 		app.display();
 	}
